d659b5caa41e326796057cd5c8b8a384bce45ffa
sduhelper <sduhelper@proton.me>
2023-03-07 09:13:13 +0000

    Add home screen and other function icons.

---
+++ app/src/main/java/com/shadai/shadaiHelper/Study.java
@@ -0,0 +1,72 @@
+package com.shadai.shadaiHelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Study#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Study extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Study() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment study.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Study newInstance(String param1, String param2) {
+        Study fragment = new Study();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_study, container, false);
+        ImageButton b1 = view.findViewById(R.id.image_button_cal2ics);
+        b1.setOnClickListener(view0 -> {
+            Log.i("I", "onCreateView: click image_button_cal2ics");
+            Intent myIntent = new Intent(view0.getContext(), ActivityCalendar2ics.class);
+            startActivity(myIntent);
+        });
+        return view;
+    }
+}
